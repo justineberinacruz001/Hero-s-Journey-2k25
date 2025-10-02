d3ad44934abec4133e0de4f112a976c0c77f682a
justine cruz <justineberinacruz001@gmail.com>
2025-10-03 02:50:16 +0800

    first foundation

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HeroJourneyRPG from './HeroJourneyRPG.jsx'
+import './index.css' // optional if you have styles
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HeroJourneyRPG />
+  </React.StrictMode>
+)
